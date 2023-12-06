f561eec9ddd7c1877547e6dcabcb07dce243980b
kawsar1kk <151685591+kawsar1kk@users.noreply.github.com>
2023-12-05 21:02:59 -0500

    first commit to my new branch

--- practiceGitWithJava/src/practiceGit/GitPractice.java
+++ practiceGitWithJava/src/practiceGit/GitPractice.java
@@ -3,6 +3,7 @@ package practiceGit;
 public class GitPractice {
 
 	public static void main(String[] args) {
-		System.out.println("first git ");
+		System.out.println("first git !! ");
+		System.out.println("github!");
 	}
 }
