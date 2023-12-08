f2f8ef24282a2fbf03862660cad0603e46591659
kawsar1kk <151685591+kawsar1kk@users.noreply.github.com>
2023-12-07 19:52:05 -0500

    new changes on class

--- practiceGitWithJava/src/practiceGit/GitPractice.java
+++ practiceGitWithJava/src/practiceGit/GitPractice.java
@@ -3,11 +3,6 @@ package practiceGit;
 public class GitPractice {
 
 	public static void main(String[] args) {
-		System.out.println("first git ");
-		
-		int o = 3;
-		
-		System.out.println("im new qa tester !");
-		System.out.println("doesnt work ");
+		System.out.println("sisis");
 	}
 }
